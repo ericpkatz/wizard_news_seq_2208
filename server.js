2f9f9416167644c2621507cdb74a8bea211c55ae
ericpkatz <ericpkatz@gmail.com>
2022-08-10 11:29:35 -0400

    a working app

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize');
 const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/wizard_news_seq_2208_db');
+const express = require('express');
+const app = express();
 
 const User = conn.define('user', {
   name: {
@@ -18,6 +20,96 @@ const Post = conn.define('post', {
 Post.belongsTo(User);
 User.hasMany(Post);
 
+app.use('/assets', express.static('assets'));
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/posts', async(req, res, next)=> {
+  try {
+    const post = await Post.create(req.body);
+    res.redirect(`/posts/${post.id}`);
+
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.get('/', async(req, res, next)=> {
+  try {
+    const posts = await Post.findAll({
+      include: [ User ]
+    });
+
+    const users = await User.findAll();
+    res.send(`
+      <html>
+        <head>
+          <title>Wizard News Seq</title>
+          <link rel='stylesheet' href='/assets/style.css' />
+        </head>
+        <body>
+          <h1>Wizard News Seq</h1>
+          <main>
+            <ul>
+              ${
+                posts.map( post => {
+                  return `
+                    <li>
+                      <a href='/posts/${post.id}'>${ post.title }</a> by ${ post.user.name }
+                    </li>
+                  `;
+                }).join('')
+              }
+            </ul>
+            <form method='post' action='/posts'>
+              <input name='title' placeholder='title' />
+              <input name='content' placeholder='content' />
+              <select name='userId'>
+                ${
+                  users.map( user => {
+                    return `
+                      <option value='${ user.id }'>${ user.name }</option>
+                    `;
+                  }).join('')
+                }
+              </select>
+              <button>Create</button>
+            </form>
+          </main>
+        </body>
+      </html>
+    `);
+
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.get('/posts/:id', async(req, res, next)=> {
+  try {
+    const post = await Post.findByPk( req.params.id, {
+      include: [ User ]
+    });
+    res.send(`
+      <html>
+        <head>
+        </head>
+        <body>
+          <h1>Wizard News Seq</h1>
+          <a href='/'>All Posts</a>
+          <h2>${ post.title } by ${ post.user.name }</h2>
+          <p>${ post.content }</p>
+        </body>
+      </html>
+    `);
+
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
 const start = async()=> {
   try {
     console.log('starting');
@@ -29,6 +121,8 @@ const start = async()=> {
     await Post.create({ title: 'foo', content: 'this is the foo content', userId: moe.id});
     await Post.create({ title: 'bar', content: 'this is the bar content', userId: moe.id});
     await Post.create({ title: 'bazz', content: 'this is the bazz content', userId: lucy.id});
+    const port = process.env.PORT || 3000;
+    app.listen(port, ()=> console.log(`listening on port ${port}`));
   }
   catch(ex){
     console.log(ex);
